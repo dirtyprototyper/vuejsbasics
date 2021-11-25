fbcb904753f4ebd4cfc05e2434320a622708e295
dirtyprototyper <dirtyprototyping@gmail.com>
2021-11-25 09:25:44 +0800

    working for mutation and getters

--- src/components/vuexdata/statedata.vue
+++ src/components/vuexdata/statedata.vue
@@ -1,25 +1,41 @@
 <template>
   <!--vue state, counter -->
   <div style="border: 1px solid black">
-    <p>{{ count }}</p>
+    <p>Normal Counter:{{ count }}</p>
+    <p>With Counter with Getters :{{ doublecounter }}</p>
 
     <button @click="increment">+1</button>
-    <button @click="decrement">-1</button>
+    <button @click="decrements">-1</button>
     <br />
     <!-- <button @click="increment2">+2</button> -->
     <!--  <button @click="decrement2">-2</button> -->
+    <button @click="doneTodosCount">-2</button>
   </div>
   <br />
   <br />
 
   <div style="border: 1px solid black; margin-top 2%">
-    <form @submit.prevent="saveData">
-      <p>
-        <label for="friend">Friend Name</label>
-        <input id="friend" v-model="friend" type="text" name="friend" />
-      </p>
+    <span>
+      The input hardcode to the state (array of object's friends friend).
+    </span>
+    <br />
 
-      <!-- <p>
+    <div style="border: 1px solid black; margin-top 2%">
+      <br />
+      <form @submit.prevent="saveData">
+        <!-- <li v-for="(item, index) in friends" :key="item.friend">
+          <input @input="updateValue('friend', index, $event.target.value)" />
+        </li> -->
+
+        <label for="friend">Friend Name</label>
+        {{ friends.friend }}
+        <input
+          @input="updateValue(0, $event.target.value)"
+          type="text"
+          name="friend"
+        />
+
+        <!-- <p>
         <label for="age">Age</label>
         <input id="age" v-model="age" type="number" name="age" min="0" />
       </p>
@@ -33,16 +49,61 @@
         </select>
       </p> -->
 
-      <p>
-        <input type="submit" value="Submit" />
-      </p>
-    </form>
+        <p>
+          <input disabled type="submit" value="Submit" />
+        </p>
+      </form>
+    </div>
+
+    <br />
+    <br />
+    <br />
+
+    <span> 2 Way Data Binding </span> <br />
+    <div style="border: 1px solid black; padding: 2%">
+      <!--take note that the key need to be the index, otherwise you cannot bind to v-model as it loses focus after 1 characater-->
+      <!-- https://stackoverflow.com/questions/43074144/vue-js-input-field-loses-its-focus-after-entry-of-one-character -->
+      <li v-for="(item, index) in friends" :key="index">
+        {{ index }}- {{ item.friend }}
+        <!-- <input v-model="item.friend" /> -->
+        <input v-model="item.friend" />
+
+        {{ item.age }}
+        <input v-model="item.age" type="number" />
+        {{ item.type }}
+        <label for="type">type of friend</label>
+        <select v-model="item.type">
+          <option>Primary</option>
+          <option>secondary</option>
+          <option>Others</option>
+        </select>
+      </li>
+      <br />
+
+      {{ friends }}
+    </div>
+  </div>
+
+  <br />
+  <br />
+  <!-- using getters to return data with that particular property-->
+  Vuex Getter
+  <div style="border: 1px solid black">{{ doneTodosCount }}</div>
+
+  <br />
+  <br />
+  cart for actions
+  <div style="border: 1px solid black">
+    {{ cart }}
+    <input v-model="cartitem" type="string" />
+    <button @click="additem">add</button>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import { store } from "../../store/store.js";
+import { mapMutations } from "vuex";
 
 export default {
   name: "Statedata",
@@ -59,31 +120,79 @@ export default {
     // cannot use mapstate with set. If not there will be warn. Take note: by default, read only, unless there is set
     // ...mapState(["friend"]),
 
-    friend: {
+    //commented temp
+    friends: {
       get() {
-        return store.state.friend;
+        return store.state.friends;
       },
 
       set(value) {
+        print(value);
         store.commit("savedata", value);
+      },
+    },
+
+    //getters
+    doneTodosCount() {
+      return store.getters.doneTodos;
+    },
+    doublecounter() {
+      return store.getters.doubleCount;
+    },
+
+    cart: {
+      get() {
+        return store.state.cart;
+      },
+
+      set(val) {
+        store.commit("savecart", val);
+      },
+    },
+
+    cartitem: {
+      get() {
+        return store.state.cartitem;
+      },
+
+      set() {
+        // store.commit("cartitem");
       },
     },
   },
 
-  //https://blog.logrocket.com/using-vuex-4-with-vue-3/
-  //  https://vuex.vuejs.org/guide/state.html#getting-vuex-state-into-vue-components
-
   methods: {
-    increment() {
-      store.commit("increment");
+    updateValue(index, value) {
+      store.commit("savedata", { index, value });
     },
-    decrement() {
-      store.commit("decrement");
+
+    additem() {
+      console.log(this.cartitem);
+      store.commit("savecart", this.cartitem);
     },
-    // getDetails(value) {
-    //   console.log("getdetails")
-    //   this.friend = value;
+
+    //if its the same name. increment (component @click) to increment (in store)
+    ...mapMutations(["increment"]),
+    // increment() {
+    //   store.commit("increment");
+    // },
+
+    //if its the different name. decrements (component @click ) to decrement (in store)
+    ...mapMutations({
+      decrements: "decrement",
+    }),
+    // decrements() {
+    //   store.commit("decrement");
     // },
   },
+
+  // actions: {},
 };
+
+//https://medium.com/swlh/adding-modules-to-a-vuex-store-8afead95e5af
+//https://blog.logrocket.com/a-complete-guide-to-mapping-in-vuex/
+//https://blog.openreplay.com/learn-how-mapping-works-in-vuex
+
+//https://blog.logrocket.com/using-vuex-4-with-vue-3/
+//  https://vuex.vuejs.org/guide/state.html#getting-vuex-state-into-vue-components
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,23 @@
 // npm install vuex@next --save
 import Vuex from "vuex";
 
+/*things to test:
+1. Getters
+2. MApping
+3. Action
+*/
+
 export const store = new Vuex.Store({
   //possible properties:
   //  States are properties that store data in our Vuex store; they let us access the data from anywhere in our Vue 3 app.
   // state: {},
+
   //Mutations are functions that let us modify states
   // mutations: {},
 
   //Getters are functions that return a state, or states that have been operated on or combined with other values.
   // getters: {},
+
   //action methods that let us run mutations to modify a state.
   // actions: {},
 
@@ -20,19 +28,28 @@ export const store = new Vuex.Store({
   state: {
     count: 0,
 
-    friend: "",
+    friends: [
+      { friend: "tom", age: 12, type: "primary" },
+      { friend: "dick", age: 13, type: "secondary" },
+    ],
+
+    cart: ["apple", "orange", "pear"],
+    cartitem: "add something",
     // age: "",
     // type: "",
   },
 
+  getters: {
+    doneTodos: (state) => {
+      return state.friends.filter((x) => x.type == "primary");
+    },
+
+    doubleCount: (state) => {
+      return state.count * 2;
+    },
+  },
   /* yet to figure out how this really works
   getters: {
-    doubleCount: (state) => {
-      console.log("dc");
-      console.log(state.count);
-      console.log(state.count * 2);
-
-      return (state) => state.count * 2;
     },
   },
 */
@@ -40,9 +57,19 @@ export const store = new Vuex.Store({
     increment: (state) => state.count++,
     decrement: (state) => state.count--,
 
-    savedata: (state, friend) => {
-      console.log(state.friend);
-      state.friend = friend;
+    //state,payload only.
+    //https://next.vuex.vuejs.org/guide/mutations.html#object-style-commit
+    //for friend
+    savedata: (state, thepayload) => {
+      state.friends[thepayload.index].friend = thepayload.value;
+    },
+
+    //for cart
+    savecart: (state, items) => {
+      console.log(items);
+      console.log(state.cartitem);
+
+      state.cart.push(items);
     },
   },
 });
